fc60c0345393d7eb3b0f3f893e25841b74418aaa
OgvKishore <OgvKishore@kishore>
2016-08-15 14:08:24 +0530

    Changes in kishore desktop

---
+++ src/main/java/com/company/spring/rest/resources/SpringRestApplicationStartUp.java
@@ -0,0 +1,16 @@
+/*package com.company.spring.rest.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@SpringBootConfiguration
+public class SpringRestApplicationStartUp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringRestApplicationStartUp.class, args);
+	}
+
+}
+*/
