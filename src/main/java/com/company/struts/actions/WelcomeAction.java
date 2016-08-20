48e5d07e9389529fbed9cd74ea4b12e64a8acc1b
OgvKishore <OgvKishore@kishore>
2016-08-20 14:42:19 +0530

    spring and struts2 integration working

--- src/main/java/com/company/struts/actions/WelcomeAction.java
+++ src/main/java/com/company/struts/actions/WelcomeAction.java
@@ -1,26 +1,38 @@
 package com.company.struts.actions;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.company.hibernate.entities.BusinessDetails;
 import com.company.spring.configurations.ApplicationConfiguration;
 import com.company.spring.services.BusinessDetailsService;
 import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
 
-@SuppressWarnings("resource")
-public class WelcomeAction {
+
+public class WelcomeAction extends ActionSupport{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -629511986127852075L;
+
 	private static final Logger LOGGER = Logger.getLogger(WelcomeAction.class.getName());
 
 	private String companyName;
 
+	@Autowired
+	BusinessDetailsService businessDetailsService;
+
 	public String execute() {
 
-		AbstractApplicationContext abstractApplicationContext = new AnnotationConfigApplicationContext(
+		/*AbstractApplicationContext abstractApplicationContext = new AnnotationConfigApplicationContext(
 				ApplicationConfiguration.class);
 		BusinessDetailsService businessDetailsService = (BusinessDetailsService) abstractApplicationContext
-				.getBean("businessDetailsService");
+				.getBean("businessDetailsService");*/
 
 		BusinessDetails businessDetails = new BusinessDetails();
 		businessDetails.setId(2);
@@ -38,5 +50,15 @@ public class WelcomeAction {
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+
+	public BusinessDetailsService getBusinessDetailsService() {
+		return businessDetailsService;
+	}
+
+	public void setBusinessDetailsService(BusinessDetailsService businessDetailsService) {
+		this.businessDetailsService = businessDetailsService;
+	}
+	
+	
 
 }
