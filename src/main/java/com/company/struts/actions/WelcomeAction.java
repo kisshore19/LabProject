c688f8e2487edffdfa7e1033aa297e9ce7fde8c0
OgvKishore <OgvKishore@kishore>
2016-08-20 14:55:03 +0530

    Fully working strutss spring hibernate example

--- src/main/java/com/company/struts/actions/WelcomeAction.java
+++ src/main/java/com/company/struts/actions/WelcomeAction.java
@@ -2,23 +2,12 @@ package com.company.struts.actions;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.company.hibernate.entities.BusinessDetails;
-import com.company.spring.configurations.ApplicationConfiguration;
 import com.company.spring.services.BusinessDetailsService;
 import com.opensymphony.xwork2.Action;
-import com.opensymphony.xwork2.ActionSupport;
 
-
-public class WelcomeAction extends ActionSupport{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -629511986127852075L;
+public class WelcomeAction {
 
 	private static final Logger LOGGER = Logger.getLogger(WelcomeAction.class.getName());
 
@@ -28,11 +17,6 @@ public class WelcomeAction extends ActionSupport{
 	BusinessDetailsService businessDetailsService;
 
 	public String execute() {
-
-		/*AbstractApplicationContext abstractApplicationContext = new AnnotationConfigApplicationContext(
-				ApplicationConfiguration.class);
-		BusinessDetailsService businessDetailsService = (BusinessDetailsService) abstractApplicationContext
-				.getBean("businessDetailsService");*/
 
 		BusinessDetails businessDetails = new BusinessDetails();
 		businessDetails.setId(2);
@@ -50,15 +34,5 @@ public class WelcomeAction extends ActionSupport{
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
-
-	public BusinessDetailsService getBusinessDetailsService() {
-		return businessDetailsService;
-	}
-
-	public void setBusinessDetailsService(BusinessDetailsService businessDetailsService) {
-		this.businessDetailsService = businessDetailsService;
-	}
-	
-	
 
 }
